00d93feb81bb5d9cfa9d6f481e01e55585611193
ironhornet <tarmyrka@gmail.com>
2020-12-13 16:56:08 +0200

    added new-items-page

--- app/js/main.js
+++ app/js/main.js
@@ -15,5 +15,31 @@ $(function(){
       });
 
     
-    var mixer = mixitup('.products__inner-box');
+
+
+$(".js-range-slider").ionRangeSlider({
+    type: "double", 
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+
+    
 });
+
+$('.icon-menu').on('click', function(){
+  $('.product__item').addClass('list');
+  $('.icon-menu').addClass('active');
+  $('.icon-th').removeClass('active');
+});
+
+$('.icon-th').on('click', function(){
+  $('.product__item').removeClass('list');
+  $('.icon-th').addClass('active');
+  $('.icon-menu').removeClass('active');
+});
+
+var mixer = mixitup('.products__inner-box');
+});
+// 
